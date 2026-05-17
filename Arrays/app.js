33495fa2ea835a736a897a785a8c5502b035fcfe
Gurudev Kini <kinigurudev049@gmail.com>
2026-05-17 20:00:13 +0530

    updated loops

--- Arrays/app.js
+++ Arrays/app.js
@@ -2,3 +2,19 @@ let student1= "Guru"
 let student2= "kini"
 
 let students= ["guru","kini"]
+students[0][0]
+// in java and C++ you have to have an array with similar type but in js diffrent types of arrays are been allowed
+/* string are immutable cnant change than array is mutable they can change
+Push = add to end
+Pop= delete from end and returns it
+Unshift = add to start
+shift = delete from start and returns it 
+indexof;- returns index of something they are also case sensitive
+includes:- search for value true or false 
+ concat:- here 2 array you can merage
+ reverse:- reverse the array
+ slice:- starting index small protion of copy 
+ slice(start, end) end is not included
+ splice :- one in all tools removes/ replcaes/ addd eleemtnes in place
+ splice(start,deleteCount, item0, itemN)
+ changes is done in the ogrinal */
---
+++ Arrays/practice/app.js
@@ -0,0 +1,9 @@
+// let start;
+// start = ['january','july','march','august']
+// start.splice(1,0, 'june');
+// start
+let javascript
+javascript = ['c','c++','html','javascript','python','java','c#','sql']
+javascript.reverse()
+console.log(javascript);
+console.log(javascript.indexOf('javascript'))
