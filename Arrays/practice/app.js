aae0645071c9091cd773f833a7969c6e97bfd965
Gurudev Kini <kinigurudev049@gmail.com>
2026-05-18 21:27:26 +0530

    updated loops

--- Arrays/practice/app.js
+++ Arrays/practice/app.js
@@ -2,8 +2,54 @@
 // start = ['january','july','march','august']
 // start.splice(1,0, 'june');
 // start
-let javascript
-javascript = ['c','c++','html','javascript','python','java','c#','sql']
-javascript.reverse()
-console.log(javascript);
-console.log(javascript.indexOf('javascript'))
+// 1st one 
+// let a= [1,2,3,4,5,6]
+// console.log(a);
+// let n=3;
+// let b=a.slice(0,n);
+// console.log(b)
+
+
+//2nd one
+// let a= [1,2,3,4,5,6]
+//  console.log(a);
+// let n=3;
+// let b= a.slice(a.length-n);
+// console.log(b);
+
+//3rd one
+// let string = "";
+// if(string== "")
+// {
+//   console.log("string is empty")
+// }
+// else {
+//   console.log("string is not empty")
+// }
+
+//4th one
+// let char = `abcd`;
+// if(char[0] ===char[0].toLowerCase())
+// {
+//   console.log("Charcter is in lower case");
+// }
+// else
+// {
+//   console.log("charcter is in upper case");
+// }
+
+
+//5th one
+let string = "Gurudev  ";
+console.log(string.trim());
+
+//6thone
+let char = ["this", "is","the"," final","quesion"]
+//search is ?
+if(char.includes("is"))
+{
+  console.log("Element in the array");
+}
+else{
+  console.log("Not fpund");
+}
