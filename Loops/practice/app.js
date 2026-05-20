c0fb94d985322493d9bcbc8ffd33b3e676de4656
Gurudev Kini <kinigurudev049@gmail.com>
2026-05-20 18:45:07 +0530

    learned return and scope concepts

--- Loops/practice/app.js
+++ Loops/practice/app.js
@@ -1,29 +1,22 @@
-//  // Question 1
+// Question 1
 // let arr = [1, 2, 3, 4, 5, 6, 2, 3]
+//   let req = parseInt(prompt("enter the Number to be deleted from the Array "))
 
-//  let req = parseInt(prompt("enter the Number to be deleted from the Array "))
+// for(let i=0; i< arr.length; i++)
+// {
+//   if(arr[i]==req)
+// {
+//   arr.slice(i,1);
+// }
+// }
+// console.log(arr);
 
-//   if(req == arr.includes == true)
-//   {
-//     num = indexOf(req);
-//     arr.splice(num);
-//     console.log("Number is Deleted")
-//   }
-//   else {
-//     console.log("Number is not there ")
-//   }
 
-// // Question 2
-// let num = 2913;
-// let count = num.length-1;
-
-// Question 3
-// let num = 2379248;
-// let sum = num[0]+num[1]+num[2]+ num[i-1];
-
-// //Question 4
-// let num=5;
-// let factorial= num[5]*num[4]*num[n-2]*num[n-1];
-
-// let arr = [1,2,4,9,23,34,54,1,23,44,35,67,78,3,4,5,2,6,9]
-// let largest= arr[0]>arr[1]>arr[2]
+let n=5;
+let fact =1;
+  for(let i=1; i<=n; i++)
+  {
+    fact=fact*i;
+    
+  }
+  console.log(fact);
