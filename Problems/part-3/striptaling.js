c952dfb37d783a7268a8831bd27fadc7ef17ccae
Gurudev Kini <kinigurudev049@gmail.com>
2026-06-21 19:46:04 +0530

    Updated

--- Problems/part-3/striptaling.js
+++ Problems/part-3/striptaling.js
@@ -1,6 +1,13 @@
-
-
-
+let array1 = ["hell0","a",23,45,99,0];
+let item =53;
+if(arr.indexOf(item) !=-1)
+{
+  console.log("element exists in array");
+}
+else 
+{
+  console.log("Element doesn't exist in array");
+}
 // let str= prompt("please Enter the String");
 // console.log(`orginal String = ${str}`);
 // console.log(`string with trim sapces = ${str.trim()}`)
--- Problems/part-4/occ.js
+++ Problems/part-4/occ.js
@@ -1,25 +1,35 @@
 // Number of Occurrences
-arr = [1, 2, 3, 4, 5, 6, 2, 3];
-num = 2;
-
-
-//find the no of digits
-number = 287152;
-console.log(number.length);
-
-// Sum of digits
-sum=0;
- for (i=0; i<number.length; i++);
+let arr = [1, 2, 2, 3];
+let num = 2;
+for(let i =0; i<arr.length; i++ )
 {
-  sum + number[0];
+  if(arr[i]==num)
+  {
+    arr.splice(i,1)
+    i--;
+  }
 }
 
 
-// Factioral
-let fact 
 
-// lagrest number of array
-let num= [2,3,4,5,6,7,8]
- let largest = num[0];
+//find the no of digits
 
-for(i=0; num[i]>num.largest )
+number = 287152;
+console.log(number.length);
+
+// // Sum of digits
+// sum=0;
+//  for (i=0; i<number.length; i++);
+// {
+//   sum + number[0];
+// }
+
+
+// // Factioral
+// let fact 
+
+// // lagrest number of array
+// let num= [2,3,4,5,6,7,8]
+//  let largest = num[0];
+
+// for(i=0; num[i]>num.largest )
